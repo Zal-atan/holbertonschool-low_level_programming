a9b94065084c0f350832ea42a449f43f7217e0c0
Zal-atan <ethanzalta@yahoo.com>
2023-05-24 12:45:52 -0700

    Adding file 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int num;
+
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
