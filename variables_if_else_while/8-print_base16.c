bc87489f8b70c0e6921e2f55995691ff400acdd9
Zal-atan <ethanzalta@yahoo.com>
2023-05-24 15:26:06 -0700

    quick fix

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int num;
+	char ch;
+
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(48 + num);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
