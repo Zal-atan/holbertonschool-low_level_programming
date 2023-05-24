6724d1e784fb2a9c79adf107b3d2dab2152c8354
Zal-atan <ethanzalta@yahoo.com>
2023-05-24 09:09:24 -0700

    Adding file 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
+
