1345c2316dbb345a5e9a948fd8459dbdf46592b3
Zal-atan <ethanzalta@yahoo.com>
2023-05-24 12:26:58 -0700

    Adding file 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char ch;
+
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			;
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
