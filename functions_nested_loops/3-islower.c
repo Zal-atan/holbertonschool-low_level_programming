8883f5fe60a2d730275514e510f845291b69e434
Zal-atan <ethanzalta@yahoo.com>
2023-05-25 08:53:03 -0700

    Adding file 3, lower case checker

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _islower - Checks if thew character is lowercase
+ *
+ * @c: - the input letter or number
+ *
+ * Return: 1 if there character is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	int l = c;
+	int i;
+	int r = 0;
+
+	for  (i = 97 ; i < 123; i++)
+	{
+		if (i == l)
+		{
+			r = 1;
+		}
+	}
+	return r;
+
+}
