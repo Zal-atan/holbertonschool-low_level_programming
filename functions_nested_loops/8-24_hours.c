ce074ee3bd861e014c798767959d6fe319585ec7
Zal-atan <ethanzalta@yahoo.com>
2023-05-25 11:14:58 -0700

    quick fix

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,7 +9,6 @@
  *
  *
  *
- * 
  *
  */
 
@@ -23,7 +22,7 @@ void jack_bauer(void)
 	while (a <= 2)
 	{
 		if (a < 2)
-		{	
+		{
 			b = 0;
 			while (b <= 9)
 			{
@@ -48,29 +47,29 @@ void jack_bauer(void)
 		a++;
 		}
 		if (a == 2)
-                {
-                        b = 0;
-                        while (b <= 3)
-                        {
-                                c = 0;
-                                while (c <= 5)
-                                {
-                                        d = 0;
-                                        while (d <= 9)
-                                        {
-                                                _putchar(a + 48);
-                                                _putchar(b + 48);
-                                                _putchar(':');
-                                                _putchar(c + 48);
-                                                _putchar(d + 48);
-                                                _putchar('\n');
-                                                d++;
-                                        }
-                                        c++;
-                                }
-                                b++;
-                        }
-                a++;
+		{
+			b = 0;
+			while (b <= 3)
+			{
+				c = 0;
+				while (c <= 5)
+				{
+					d = 0;
+					while (d <= 9)
+					{
+						_putchar(a + 48);
+						_putchar(b + 48);
+						_putchar(':');
+						_putchar(c + 48);
+						_putchar(d + 48);
+						_putchar('\n');
+						d++;
+					}
+					c++;
+				}
+				b++;
+			}
+		a++;
 		}
 	}
 
