d0fb1142d356497d32acfcd1e6db0d5f24b2ef9d
Zal-atan <ethanzalta@yahoo.com>
2023-05-25 12:10:39 -0700

    Adding file 9 - Times Table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * times_table - This function displays a multiplication table
+ *
+ *
+ *
+ * Return: Returns the value of the last digit
+ *
+ */
+
+void times_table(void)
+{
+	int x, y, prod;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0 ; y <= 9; y++)
+		{
+			prod = (x * y);
+			if (prod < 10)
+			{
+				_putchar(' ');
+			}
+			else if (prod >= 10)
+			{
+				_putchar('0' + (prod / 10));
+			}
+			_putchar((prod % 10) + '0');
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
+
+
+		}
+	}
+
+
+}
