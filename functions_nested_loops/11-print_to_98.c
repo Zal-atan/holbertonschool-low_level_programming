0f7cc8e91a0af7d319b1bb1f8325731c50236b1d
Zal-atan <ethanzalta@yahoo.com>
2023-05-26 06:33:59 -0700

    Adding file 11 - print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_to_98 - prints each number from given number to 98
+ *
+ * @n: - the first input number
+ *
+ */
+
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		printf("%d, ", n);
+		putchar('\n');
+	}
+
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+				n--;
+		}
+		putchar('\n');
+	}
+
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+				n++;
+		}
+		putchar('\n');
+	}
+
+
+}
