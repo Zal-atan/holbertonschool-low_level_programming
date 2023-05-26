279cb7ed3864b34b5989fa4536597efd660a476a
Zal-atan <ethanzalta@yahoo.com>
2023-05-26 07:48:56 -0700

    Adding file 10 - Advanced Task - to variables directory

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int a, b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = (a + 1); b <= 9 ; b++)
+		{
+			putchar(a + 48);
+			putchar(b + 48);
+			putchar(',');
+			putchar(' ');
+		}		
+	}
+	putchar('\n');
+	return (0);
+}
