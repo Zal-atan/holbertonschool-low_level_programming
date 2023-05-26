8def5e65cd36836423dbadf6cb8e5a7285111e8f
Zal-atan <ethanzalta@yahoo.com>
2023-05-26 06:15:25 -0700

    Adding file 10, sum

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * add - returns the sum of n1 and n2
+ *
+ * @n1: - the first input number
+ *
+ * @n2: - the second input number
+ *
+ * Return: Returns the value of the sum of n1 and n2
+ *
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+
+}
