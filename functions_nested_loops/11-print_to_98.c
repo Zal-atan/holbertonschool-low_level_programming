1f14e09a4278fd9bc342e3a58de30900709735b5
Zal-atan <ethanzalta@yahoo.com>
2023-05-26 06:36:41 -0700

    quick fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -15,28 +15,27 @@ void print_to_98(int n)
 {
 	if (n == 98)
 	{
-		printf("%d, ", n);
-		putchar('\n');
+		printf("%d\n", n);
 	}
 
 	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 				n--;
 		}
-		putchar('\n');
+		printf("%d\n", n);
 	}
 
 	else if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 				n++;
 		}
-		putchar('\n');
+		printf("%d\n", n);
 	}
 
 
