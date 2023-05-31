a6d4a0b38f91c7fe43fc137afae5db7bd472f830
Zal-atan <ethanzalta@yahoo.com>
2023-05-31 11:10:37 -0700

    Adding file 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _puts - Prints the length of the string
+ *
+ * @str: string input
+ *
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' ; i++)
+		_putchar(str)
+}
