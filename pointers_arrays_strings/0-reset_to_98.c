c19f02ddb8f2d41efa9d445d5f2c2496ec4fbe6b
Zal-atan <ethanzalta@yahoo.com>
2023-05-31 10:51:10 -0700

    Added dir Pointers Array and Strings, Added Readme, Added file 0, added main.h

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * reset_to_98 - Changes pointer value to 98
+ *
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
