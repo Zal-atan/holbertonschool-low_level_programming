1dc5f9c669f86fccc812786ba31a9955059148b6
Zal-atan <ethanzalta@yahoo.com>
2023-05-31 09:20:31 -0700

    Adding file 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_diagonal - prints line of length i
+ *
+ * @n: length of line to print
+ *
+ * Return: returns nothing
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0, j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+
+
+}
