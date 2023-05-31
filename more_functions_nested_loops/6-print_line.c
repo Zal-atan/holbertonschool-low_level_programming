847e7cf9867e9ed5be887e6ec64007490eea0ab8
Zal-atan <ethanzalta@yahoo.com>
2023-05-31 09:10:03 -0700

    Adding file 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_line - prints line of length i
+ *
+ * @n: length of line to print
+ *
+ * Return: returns nothing
+ *
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+
+
+}
