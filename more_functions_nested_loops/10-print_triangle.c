25cd7b0ad659b282d8cadb8a5cdf4f39cf8ec054
Zal-atan <ethanzalta@yahoo.com>
2023-05-31 10:06:54 -0700

    Adding file 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_triangle - prints square of length n
+ *
+ * @n: length and height of square
+ *
+ * Return: returns nothing
+ *
+ */
+
+void print_triangle(int n)
+{
+	int i, j, k;
+
+	for (i = 0; i < n; i++)
+	{
+		k = (n - i)
+		for (j = 0; j < n; j++)
+		{
+			if (j < k)
+				_putchar('#');
+			else
+				_putchat(' ');
+		}
+		if (i < (n - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+
+}
