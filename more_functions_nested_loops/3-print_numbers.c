9ea9b5ff47b79a82965a2226ea68467269d89ec4
Zal-atan <ethanzalta@yahoo.com>
2023-05-31 08:24:47 -0700

    Adding file 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ *
+ * Return: returns nothing
+ *
+ */
+
+void print_numbers(void);
+{
+	int i;
+
+	for (i = 0, i <= 9, i++)
+	{
+		_putchar(i + '0')
+	}
+	return(0)
+
+}
