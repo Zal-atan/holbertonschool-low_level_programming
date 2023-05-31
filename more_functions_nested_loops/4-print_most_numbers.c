9c6602f262e565fab3bc595f9e62f2dd5019e9c3
Zal-atan <ethanzalta@yahoo.com>
2023-05-31 08:36:52 -0700

    quick fix

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -18,8 +18,9 @@ void print_most_numbers(void)
 	for (i = 0; i <= 9; i++)
 	{
 		if ((i == 2) || (i == 4))
-			break;
-		_putchar(i + '0');
+			;
+		else
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 
