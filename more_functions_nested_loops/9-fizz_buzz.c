d553446b30b67443e88f77016410993a7e1cbb25
Zal-atan <ethanzalta@yahoo.com>
2023-05-31 09:41:43 -0700

    Adding file 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - prints fizz buzz
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
