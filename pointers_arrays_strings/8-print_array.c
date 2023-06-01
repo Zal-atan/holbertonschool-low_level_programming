09f19aa3fa2339b43bfc668fd6d8797b17401e19
Zal-atan <ethanzalta@yahoo.com>
2023-06-01 10:48:22 -0700

    Adding number 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_array - Prints an array number by number
+ *
+ * @a: array name
+ *
+ * @n: number of items to print in an array
+ *
+ * Return: none
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+			printf(", ");
+		else
+			printf("\n")
+	}
+}
