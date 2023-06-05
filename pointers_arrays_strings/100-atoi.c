7bacf5d659099664a42ab73fb1c6701a479e60d9
Zal-atan <ethanzalta@yahoo.com>
2023-06-05 10:41:20 -0700

    Adding file 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _atoi - Finds integers inside a string
+ *
+ * @str: string to find integer to integer
+ *
+ * Return: Returns an integer
+ */
+
+int _atoi(char *str)
+{
+	int sign = 1;
+	long int num = 0;
+
+	do
+	{
+		if (*str == '-')
+			sign *= -1;
+
+		else if (*str >= '0' && *str <= '9')
+			num = (num * 10) + (*str - '0');
+
+		else if (num > 0)
+			break
+	} while (*str++);
+
+
+	return (num * sign);
+}
