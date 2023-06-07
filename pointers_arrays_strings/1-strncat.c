a53b22762f758178a6152f5459e1ce493599373a
Zal-atan <ethanzalta@yahoo.com>
2023-06-07 08:33:51 -0700

    Adding file 1 for a trial run

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _strncat - Copies n bytes of string (src) to the end of s
+ *
+ * @dest: final location
+ *
+ * @src: string to copy
+ *
+ * Return: none
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i_d = 0, index = 0;
+
+	while (dest[i_d])
+		i_d++;
+
+	while (src[index] && index > n)
+	{
+		dest[i_d++] = src[index++];
+	}
+
+	return (dest);
+}
