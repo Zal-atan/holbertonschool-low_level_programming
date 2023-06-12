15f96d1c0c0543c2aea5c012bc9b49941d41fbb2
Zal-atan <ethanzalta@yahoo.com>
2023-06-12 09:37:51 -0700

    quick fix

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -22,9 +22,9 @@ void print_diagsums(int *a, int size)
 	{
 		if (i % (size + 1) == 0)
 			sum1 += *(a + i);
-		if (i % (size - 1) == 0 && i < (size * size - 1))
+		if (i % (size - 1) == 0 && i != 0 && i < (size * size - 1))
 			sum2 += *(a + i);
 	}
 
-	printf("%ld, %ld\n", sum1,sum2);
+	printf("%ld, %ld\n", sum1, sum2);
 }
