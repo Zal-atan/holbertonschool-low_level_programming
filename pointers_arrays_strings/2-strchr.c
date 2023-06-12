df24edecf80ae82d0d9bf33fb2e66ef0a0455f87
Zal-atan <ethanzalta@yahoo.com>
2023-06-12 07:30:14 -0700

    Adding file 2 strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _strchr - returns a pointer to first occurence of charc c in str
+ *
+ * @s: string input
+ *
+ * @c: character looking for in string
+ *
+ * Return: pointer to first character c in string s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (0);
+}
+
