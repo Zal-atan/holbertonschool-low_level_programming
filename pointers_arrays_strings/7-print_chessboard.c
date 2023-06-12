e93deed546179f3821d7842be8c5290f06524f11
Zal-atan <ethanzalta@yahoo.com>
2023-06-12 09:09:38 -0700

    Adding file 7 print chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -5,39 +5,21 @@
 /* more headers goes there */
 
 /**
- * _strstr - finds first occurrence of str 2 in str 1
+ * print_chessboard - prints a chessboard
  *
- * @haystack: string to search in
+ * @a: an array of the chessboard
  *
- * @string to search for
  *
- * Return: Returns pointer to where second str appears in str 1
  */
 
-char *_strstr(char *haystack, char *needle)
+void print_chessboard(char (*a)[8])
 {
-	int i, j, k;
-	char *c;
+	int i, j;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i] == needle[j])
-			{
-				c = (haystack + i);
-				for (; needle[j] != '\0'; j++, i++)
-					if (haystack[i] != needle[j])
-						k = 0;
-					else if (haystack[i] == needle[j])
-						k = 1;
-				if (k == 0)
-					break;
-				else if (k == 1)
-					return (c);
-			}
-		}
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
 	}
-	return (0);
 }
-
