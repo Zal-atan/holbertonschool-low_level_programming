14958b8e32dbb8d6068c6e75ff6810c24eaa7c1f
Zal-atan <ethanzalta@yahoo.com>
2023-06-21 13:43:57 -0700

    Adding file 1, create an array with malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array in memory using input length and charcter
+ *
+ * @size: size of the array
+ *
+ * @c: char to input into the array
+ *
+ * Return: Returns pointer to the array, or NULL if it fails
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i = 0;
+
+	ar = malloc(sizeof(*ar) * size);
+	if (ar == NULL)
+		return NULL;
+	while (i < size)
+	{
+		ar[i] = c;
+		i++;
+	}
+	return (ar);
+}
