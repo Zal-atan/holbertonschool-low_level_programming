ae3ddb82b7841fc266b3180ac60eb12f8a1acb1e
Zal-atan <ethanzalta@yahoo.com>
2023-06-21 13:46:16 -0700

    quick fix

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -16,6 +16,8 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 	unsigned int i = 0;
 
+	if (size == 0)
+		return (NULL);
 	ar = malloc(sizeof(*ar) * size);
 	if (ar == NULL)
 		return (NULL);
