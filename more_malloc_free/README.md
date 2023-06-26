e907a4f1cca293373d57d0855f64c41d922a00b9
Zal-atan <ethanzalta@yahoo.com>
2023-06-26 11:54:27 -0700

    Adding new repo, readme, main.h, and putchar

---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+In this directory we are working more on malloc
---
+++ more_malloc_free/main.h
@@ -0,0 +1,17 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+
+#endif
