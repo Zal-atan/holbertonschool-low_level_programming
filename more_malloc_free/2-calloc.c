7390a9f3cf8d817d106a8adb58ec946e77c17a7b
Zal-atan <ethanzalta@yahoo.com>
2023-06-26 13:53:38 -0700

    Adding file 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocations array of nmemb elements of size bytes
+ *
+ * @nmemb: number of elements
+ *
+ * @size: size of each element
+ *
+ * Return: Returns pointer to the array, or NULL if it fails
+ *
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *pnt;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	pnt = malloc(nmemb * size);
+
+	if (pnt == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		pnt[i] = 0;
+	return (pnt);
+}
