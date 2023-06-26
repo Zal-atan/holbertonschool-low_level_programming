8915b772ccb94a0c5638262c9cfc506488cd7385
Zal-atan <ethanzalta@yahoo.com>
2023-06-26 14:02:17 -0700

    quick fix

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ar = malloc(sizeof(int) * (max - min) + 1);
+	ar = malloc(sizeof(int) * (max - min + 1);
 
 	if (ar == NULL)
 	{
