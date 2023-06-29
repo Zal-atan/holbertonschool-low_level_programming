1881834aed265951f38e36eaf6ff01b1805a7cba
Zal-atan <ethanzalta@yahoo.com>
2023-06-29 08:48:19 -0700

    Adding file 1 - init dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - Function that initiates data structure "dog"
+ *
+ * @dog: address of previously initiated structure dog
+ *
+ * @name: name of dog to add to structure
+ *
+ * @age: age of dog to add to struct
+ *
+ * @owner: name of owner to add to struct
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
