7aeacaeeed9c4521b8935e04a8ef0d944c1949a8
Zal-atan <ethanzalta@yahoo.com>
2023-06-29 08:35:49 -0700

    Adding new direcotry "Structures", added readme, added file 1 dog.h

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+In this project, we will be working with structures and typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
