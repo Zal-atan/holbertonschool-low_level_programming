9bdf9665938568842295a61e9102d8849b7f62b6
Zal-atan <ethanzalta@yahoo.com>
2023-07-03 09:56:52 -0700

    Adding file 1 array iterator

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -3,11 +3,20 @@
 /**
  * array_iterator - executes given function on each member of array
  *
- * @array - array of integers passed in
+ * @array: array of integers passed in
  *
- * @size - size of the array
+ * @size: size of the array
  *
- * @action - pointer to function which executes on the array
+ * @action: pointer to function which executes on the array
  */
 
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
 
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
