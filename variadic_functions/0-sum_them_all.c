a6692535813b55912aa83f751410000bce385852
Zal-atan <ethanzalta@yahoo.com>
2023-07-03 12:25:13 -0700

    quick fix

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
  * sum_them_all - sums all the given numbers
  *
- * @n - number of integers to add
+ * @n: number of integers to add
  *
  * Return: Returns sum of the numbers given
  */
@@ -14,7 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	va_list arg;
 
-	va_start (arg, n);
+	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(arg, int);
