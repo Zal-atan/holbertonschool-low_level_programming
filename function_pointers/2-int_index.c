90cd8f170bc8c77ff27a1b60059be950988385c1
Zal-atan <ethanzalta@yahoo.com>
2023-07-03 10:11:35 -0700

    quick fix

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -17,6 +17,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size <= 0)
+		return (-1);
+
 	if (array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
@@ -26,5 +29,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		}
 	}
 
-	return (0);
+	return (-1);
 }
