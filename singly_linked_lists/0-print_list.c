5fac0d0c83d00c8cf3a01e64254c6642bc26149c
Zal-atan <ethanzalta@yahoo.com>
2023-07-12 10:48:33 -0700

    Making betty compliant

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,21 +8,20 @@
  * Return: Returns the number of nodes
  */
 
- size_t print_list(const list_t *h)
- {
-    const list_t *temp = h;
-    unsigned int i = 0;
+size_t print_list(const list_t *h)
+{
+	const list_t *temp = h;
+	unsigned int i = 0;
 
-    while (temp)
-    {
-        if (!((*temp).str))
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", (*temp).len, (*temp).str);
-        
-        i++;
-        temp = (*temp).next;
-    }
+	while (temp)
+	{
+		if (!((*temp).str))
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", (*temp).len, (*temp).str);
 
-    return (i);
- }
+		i++;
+		temp = (*temp).next;
+	}
+	return (i);
+}
