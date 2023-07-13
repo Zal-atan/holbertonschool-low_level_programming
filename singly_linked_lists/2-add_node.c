f136cf7d414c9ab3fd1ec2919325bfb3534350e9
Zal-atan <ethanzalta@yahoo.com>
2023-07-13 09:18:43 -0700

    Adding filde 2, add to front

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - Prints all elements of list_t
+ *
+ * @head: current head of list
+ *
+ * @str: string to add to list
+ *
+ * Return: Returns the number of nodes
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *addNew;
+
+	addNew = malloc(sizeof(list_t));
+
+	if (addNew == NULL)
+	{
+		free(addNew);
+		return (NULL);
+	}
+
+	(*addNew).str = strdup(str);
+	(*addNew).len = strlen(str);
+	(*addNew).next = *head;
+	*head = addNew;
+
+	return (addNew);
+}
