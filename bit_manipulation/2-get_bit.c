2722dec259afb07972f4691d5d7ab8f7dfc65e6a
Zal-atan <ethanzalta@yahoo.com>
2023-07-14 09:17:34 -0700

    Adding file 3 - get _bit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * get_bit - gets value of the bit at specific place
+ *
+ * @n: input number
+ * 
+ * @index: digit looking for
+ *
+ * Return: Returns 1 or 0 or -1 if failed
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int x;
+
+	if (index > 63)
+		return (-1);
+
+	x = n >> index;
+
+	return (x & 1);
+}
