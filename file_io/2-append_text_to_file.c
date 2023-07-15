88d9b6ba241a02e86c794b04f30e453332c4f005
Zal-atan <ethanzalta@yahoo.com>
2023-07-15 11:03:11 -0700

    quick fix

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	openf = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	openf = open(filename, O_WRONLY | O_APPEND);
 	if (openf == -1)
 		return (-1);
 
