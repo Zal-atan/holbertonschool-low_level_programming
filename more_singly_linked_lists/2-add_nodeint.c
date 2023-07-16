99579f095159429f9d069065f14c61513d346c2e
Zal-atan <ethanzalta@yahoo.com>
2023-07-16 08:53:23 -0700

    Adding file 2 - Add_nodeint

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - Adds a node at the head
+ *
+ * @head: current head of list
+ *
+ * @str: string to add to list
+ *
+ * Return: Returns the number of nodes
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *addNew;
+
+	addNew = malloc(sizeof(listint_t));
+
+	if (addNew == NULL)
+	{
+		free(addNew);
+		return (NULL);
+	}
+
+	(*addNew).n = n;
+	(*addNew).next = *head;
+	*head = addNew;
+
+	return (addNew);
+}
