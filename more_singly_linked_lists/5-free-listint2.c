a4115c4be9ad4b284a4d35c91c962b2e58351e1c
Zal-atan <ethanzalta@yahoo.com>
2023-07-16 09:13:25 -0700

    quick fix

---
+++ more_singly_linked_lists/5-free-listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the memory from the list
+ *
+ * @head: current head of list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *pos;
+
+	if (!head)
+	{
+		return;
+	}
+
+	free_listint(head);
+	*head = NULL;
+}
