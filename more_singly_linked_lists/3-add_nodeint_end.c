2a1f2b52c9432314516f73ec2936b62f6ffa30d9
Zal-atan <ethanzalta@yahoo.com>
2023-07-16 08:58:41 -0700

    Adding file 3 - and_nodeint_end

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Adds new integer node to end of list
+ *
+ * @head: current head of list
+ *
+ * @n: integer to add to the list
+ *
+ * Return: Returns the number of nodes
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	list_t *addNewEnd;
+	list_t *pos = *head;
+
+	addNewEnd = malloc(sizeof(list_t));
+
+	if (addNewEnd == NULL)
+	{
+		free(addNewEnd);
+		return (NULL);
+	}
+
+	(*addNewEnd).n = n;
+	(*addNewEnd).next = NULL;
+
+	if (*head == NULL)
+		*head = addNewEnd;
+
+	else
+	{
+		while (pos->next)
+			pos = ((*pos).next);
+
+		(*pos).next = addNewEnd;
+	}
+
+	return (addNewEnd);
+}
