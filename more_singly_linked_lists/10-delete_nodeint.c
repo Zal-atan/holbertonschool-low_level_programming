4c5ac833dc23c8ea548fa6b64ab40f445f0819a2
Zal-atan <ethanzalta@yahoo.com>
2023-07-16 10:08:44 -0700

    Task 10 First try

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a node at position (index)
+ *
+ * @head: current head of list
+ *
+ * @index: position of node to delete
+ *
+ * Return: Returns 1 on success or -1 on failure
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp = *head;
+	unsigned int i = 0;
+	listint_t *delete_node;
+
+
+
+	if (index == 0)
+	{
+		*head = (**head).next;
+		free(temp);
+		return (1);
+	}
+
+	while (i < (index - 1))
+	{
+		if ((*temp).next == NULL)
+			return (-1);
+
+		temp = (*temp).next;
+		i++;
+	}
+
+	delete_node = temp->next;
+	(*temp).next = (*delete_node).next;
+	free(delete_node);
+
+	return (1);
+}
