ec43038e77dd546ef934222ea1a38a12c0e4cc1d
Zal-atan <ethanzalta@yahoo.com>
2023-07-22 10:28:38 -0700

    Adding file 2 - add_dnodeint

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Adds a node at the head
+ *
+ * @head: current head of list
+ *
+ * @n: integer to add to the node
+ *
+ * Return: Returns the number of nodes
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *addNew;
+
+	addNew = malloc(sizeof(dlistint_t));
+
+	if (addNew == NULL)
+	{
+		free(addNew);
+		return (NULL);
+	}
+
+	(*addNew).n = n;
+	(*addNew).next = *head;
+	(*addNew).prev = NULL;
+	*head = addNew;
+
+	return (addNew);
+}
