d1828ae9b35d3dec10c4986660a81845d260af2f
Zal-atan <ethanzalta@yahoo.com>
2023-07-22 10:23:13 -0700

    Adding file 1 - dlistint_len

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Returns number of nodes
+ * print_dlistint - Prints all elements of input list
  *
- * @h: input to header of the list
+ * @h: input list to be printed
  *
  * Return: Returns the number of nodes
  */
 
-size_t dlistint_len(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *temp = h;
 	unsigned int i = 0;
 
 	while (temp)
 	{
+		printf("%d\n", (*temp).n);
 		temp = (*temp).next;
 		i++;
 	}
