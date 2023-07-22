7b45e5f00c099a8b33b77c79a74c2532eeebc1a0
Zal-atan <ethanzalta@yahoo.com>
2023-07-22 10:24:30 -0700

    Redid number 1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all elements of input list
+ * dlistint_len - Returns number of nodes
  *
- * @h: input list to be printed
+ * @h: input to header of the list
  *
  * Return: Returns the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *temp = h;
 	unsigned int i = 0;
 
 	while (temp)
 	{
-		printf("%d\n", (*temp).n);
 		temp = (*temp).next;
 		i++;
 	}
