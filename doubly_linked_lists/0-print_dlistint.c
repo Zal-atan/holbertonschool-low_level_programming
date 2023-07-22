15bf5a11630b395a69baf9f4782940f52b594d2a
Zal-atan <ethanzalta@yahoo.com>
2023-07-22 10:19:51 -0700

    Adding file 0 - print dlistint

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all elements of input list
+ *
+ * @h: input list to be printed
+ *
+ * Return: Returns the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp = h;
+	unsigned int i = 0;
+
+	while (temp)
+	{
+		printf("%d\n", (*temp).n);
+		temp = (*temp).next;
+		i++;
+	}
+	return (i);
+}
