8d4ad51bb9a1c16cd728bb3b1feec381aa161c14
Zal-atan <ethanzalta@yahoo.com>
2023-07-22 12:42:26 -0700

    quick fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	}
 
-	while (i < (idx - 1) && temp)
+	while (i < (idx - 1))
 	{
 		if (temp == NULL)
 			return (NULL);
